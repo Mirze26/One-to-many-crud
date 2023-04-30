b157bff33c0a94850a975f41c1984b464368c163
Mirze26 <mirzaab@code.edu.az>
2023-05-01 01:49:54 +0400

    One to many

---
+++ EntityFramework-Slider/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,270 @@
+﻿using EntityFramework_Slider.Areas.Admin.ViewModels;
+using EntityFramework_Slider.Data;
+using EntityFramework_Slider.Helpers;
+using EntityFramework_Slider.Models;
+using EntityFramework_Slider.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace EntityFramework_Slider.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _env;    
+        private readonly AppDbContext _context;
+        public ProductController(IProductService productService,
+                                 ICategoryService categoryService,
+                                 IWebHostEnvironment env,
+                                AppDbContext context )
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _env = env;
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index(int page = 1, int take = 5)
+        {
+            List<Product> products = await _productService.GetPaginatedDatas(page, take);
+
+            List<ProductListVM> mappedDatas = GetMappedDatas(products);
+
+            int pageCount = await GetPageCountAsync(take);
+
+            Paginate<ProductListVM> paginatedDatas = new(mappedDatas, page, pageCount); 
+                                                                                       
+                                                                                        
+            ViewBag.take = take;
+
+            return View(paginatedDatas);
+        }
+
+        private async Task<int> GetPageCountAsync(int take)
+        {
+            var productCount = await _productService.GetCountAsync();
+            return (int)Math.Ceiling((decimal)productCount / take);  
+        }                                                             
+
+        
+        private List<ProductListVM> GetMappedDatas(List<Product> products)                   
+        {
+           
+
+            List<ProductListVM> mappedDatas = new();
+
+            foreach (var product in products)   
+            {
+                ProductListVM productsVM = new()  
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    Count = product.Count,
+                    CategoryName = product.Category.Name,
+                    MainImage = product.Images.Where(m => m.IsMain).FirstOrDefault()?.Image
+                };
+
+                mappedDatas.Add(productsVM);
+            }
+            return mappedDatas;
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+
+
+            ViewBag.categories = await GetCategoriesAsync();
+
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductCreateVM model)
+        {
+            try
+            {
+                ViewBag.categories = await GetCategoriesAsync();  
+
+
+                if (!ModelState.IsValid)
+                {
+                    return View(model); 
+                }
+
+
+                foreach (var photo in model.Photos)     
+                {
+                    if (!photo.CheckFileType("image/"))
+                    {
+
+                        ModelState.AddModelError("Photo", "File type must be image");
+                        return View();
+
+                    }
+
+                    if (!photo.CheckFileSize(200))
+                    {
+
+                        ModelState.AddModelError("Photo", "Image size must be max 200kb");
+                        return View();
+
+                    }
+                }
+                
+                List<ProductImage> productImages = new();
+
+
+                foreach (var photo in model.Photos)   
+                {
+
+
+                    string fileName = Guid.NewGuid().ToString() + "_" + photo.FileName;             
+                                                                                                      
+                                                                          
+                    string path = FileHelper.GetFilePath(_env.WebRootPath, "img", fileName);
+
+                   
+
+
+                    await FileHelper.SaveFileAsync(path, photo);   
+
+                    ProductImage productImage = new()   
+                    {
+                        Image = fileName
+                    };
+
+                    productImages.Add(productImage);   
+
+                }
+
+
+                productImages.FirstOrDefault().IsMain = true;   
+
+                decimal convertPrice = decimal.Parse(model.Price.Replace(".", ",")); 
+
+                Product newProduct = new()
+                {
+                    Name = model.Name,
+                    Price = convertPrice,
+                    Count = model.Count,
+                    Description = model.Description,
+                    CategoryId = model.CategoryId,
+                    Images = productImages
+                };
+                              
+                await _context.ProductImages.AddRangeAsync(productImages);
+                await _context.Products.AddAsync(newProduct);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+
+
+        private async Task<SelectList> GetCategoriesAsync()
+        {
+            IEnumerable<Category> categories = await _categoryService.GetAll();   
+            return new SelectList(categories, "Id", "Name");
+        }
+
+
+
+        [HttpGet] 
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null) return BadRequest();
+
+            Product product = await _productService.GetFullDataById((int)id);
+           
+            if(product == null) return NotFound();
+            ViewBag.desc = Regex.Replace(product.Description, "<.*?>", String.Empty); 
+            return View(product);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteProduct(int? id)
+        {
+            Product product = await _productService.GetFullDataById((int)id);
+
+            foreach (var item in product.Images)
+            {
+
+                string path = FileHelper.GetFilePath(_env.WebRootPath, "img", item.Image);
+
+                FileHelper.DeleteFile(path);
+            }
+
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id is null) return BadRequest();
+
+
+            Product product = await _context.Products.FindAsync(id);
+
+            if (product is null) return NotFound();
+
+
+
+            return View(product);
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+
+
+
+            if (id is null) return BadRequest();     
+
+
+            Product product = await _context.Products.FindAsync(id);
+
+            if (product is null) return NotFound();
+
+
+
+            return View(product);
+        }
+
+
+
+
+    }
+}
